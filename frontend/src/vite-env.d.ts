89a431ee32126f18314730668f535cd288be552b
Lucian <lucian.personal@foxmail.com>
2025-05-12 09:55:28 +0200

    [Fix] HTMLInputElement index.d.ts

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,3 +1,11 @@
 // vite-env.d.ts
 /// <reference types="vite-plugin-pages/client-react" />
 /// <reference types="vite/client" />
+
+// 扩展React的InputHTMLAttributes接口，增加directory和webkitdirectory属性
+declare module 'react' {
+  interface InputHTMLAttributes<T> extends HTMLAttributes<T> {
+    directory?: string;
+    webkitdirectory?: string;
+  }
+}
